19307360b7eebcd9434c8056e9b01e72e2ab4f29
austin-keith-vigo <austin.keith.vigo@gmail.com>
2021-08-03 13:24:56 -0700

    fixed bug where multiple images appear on same cell

--- NasaImages/NasaImages/source_files/ImagesCollectionViewCell.swift
+++ NasaImages/NasaImages/source_files/ImagesCollectionViewCell.swift
@@ -21,6 +21,14 @@ class ImagesCollectionViewCell: UICollectionViewCell {
         self.initViews()
     }
     
+    override func prepareForReuse() {
+        
+        // Remove previous image
+        if let previewImageView = self.previewImageView {
+            previewImageView.image = nil
+        }
+    }
+    
     func initViews() {
         self.previewImageView?.isUserInteractionEnabled = false
         self.previewImageView?.contentMode = .scaleAspectFill
@@ -29,14 +37,19 @@ class ImagesCollectionViewCell: UICollectionViewCell {
     /*
      Sets values for UIElements presenting data from image object assuming UIElements have been loaded
      */
+    var currentSequence: Int = 0
     func setInfo(image: Image) {
         guard let previewImageView = self.previewImageView else { return }
+        
+        // Makes sure the previous request on the reused cell does not appear before the current request
+        self.currentSequence += 1
+        let sequence = self.currentSequence
         
         // Asynchronously load image from cache or make request to load image
         // TODO: Implement blur background to indicate image loading
         if let url = URL(string: image.getImagePath() ?? "") {
             ImageCache.load(url: url, completionHandler: { image in
-                if let image = image {
+                if let image = image, sequence == self.currentSequence {
                     DispatchQueue.main.async {
                         previewImageView.image = image
                     }
