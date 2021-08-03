e889cd52796706c55f1704a8dea166feb5590b57
austin-keith-vigo <austin.keith.vigo@gmail.com>
2021-08-03 00:46:29 -0700

    Updated Structure of directory
    Updated Structure of directory

---
+++ NasaImages/NasaImages/supporting_files/api/NetworkConstants.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkConstants.swift
+//  NasaImages
+//
+//  Created by austin vigo on 8/1/21.
+//
+
+import Foundation
+
+class NetworkConstants {
+    static let BASE_URL: String = "https://images-api.nasa.gov/"
+}
