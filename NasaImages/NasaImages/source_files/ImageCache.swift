f98ad71e6527627994d2e23fe5ee73b122f093a5
austin-keith-vigo <austin.keith.vigo@gmail.com>
2021-08-03 13:38:35 -0700

    Redid image cache
    Redid image cache

--- NasaImages/NasaImages/source_files/ImageCache.swift
+++ NasaImages/NasaImages/source_files/ImageCache.swift
@@ -10,31 +10,30 @@ import UIKit
 
 class ImageCache {
     
+    private static var cache: NSCache = NSCache<NSString, UIImage>()
+ 
     /*
      Loads image from web asynchronosly and caches it, in case you have to load url
      again, it will be loaded from cache if available
      */
-    static func load(url: URL, cache: URLCache? = nil, completionHandler: @escaping (_ image: UIImage?) -> Void){
-        let cache = cache ?? URLCache.shared
-        let request = URLRequest(url: url)
-        if let data = cache.cachedResponse(for: request)?.data, let image = UIImage(data: data) {
+    static func load(url: URL, completionHandler: @escaping (_ image: UIImage?) -> Void){        let request = URLRequest(url: url)
+        if let image = ImageCache.cache.object(forKey: url.absoluteString as NSString) {
             completionHandler(image)
         } else {
             URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
                 if let data = data, let response = response, ((response as? HTTPURLResponse)?.statusCode ?? 500) < 300, let image = UIImage(data: data) {
-                    let cachedData = CachedURLResponse(response: response, data: data)
-                    cache.storeCachedResponse(cachedData, for: request)
+                    ImageCache.cache.setObject(image, forKey: url.absoluteString as NSString)
                     completionHandler(image)
                 }
             }).resume()
         }
     }
     
+    
     /*
      Clears all cached responses from the image cache to free up space
      */
     static func clear(_ cache: URLCache? = nil) {
-        let cache = cache ?? URLCache.shared
-        cache.removeAllCachedResponses()
+        ImageCache.cache.removeAllObjects()
     }
 }
